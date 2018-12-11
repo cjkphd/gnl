8a8d496725e8d7a39f1759cf66eb20ae5aee4fbb
Matthew Mateo <mmatteo36@gmail.com>
2018-12-10 23:41:10 -0800

    added definitions

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,16 @@
 /*   By: mamateo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 18:47:21 by mamateo           #+#    #+#             */
-/*   Updated: 2018/12/10 18:47:23 by mamateo          ###   ########.fr       */
+/*   Updated: 2018/12/10 23:40:51 by mamateo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <libft.h>
+# include <stdlib.h>
+# include <unistd.h>
 
+
+
+#endif

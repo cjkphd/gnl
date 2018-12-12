be3faf1911103f8cf6cc9a3e24eed6c4b8bbd5b1
Matthew Mateo <mmatteo36@gmail.com>
2018-12-11 22:18:39 -0800

    libft&modi

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -1,21 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mamateo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/10 18:47:21 by mamateo           #+#    #+#             */
-/*   Updated: 2018/12/11 22:12:39 by mamateo          ###   ########.fr       */
+/*   Created: 2018/12/10 18:46:41 by mamateo           #+#    #+#             */
+/*   Updated: 2018/12/10 23:24:37 by mamateo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-# include <libft.h>
-# include <stdlib.h>
-# include <unistd.h>
-
-# BUF_SIZE 4
-
-#endif
+int	get_next_line(const int fd, char **line)
+{
+}
--- libft/get_next_line.h
+++ libft/get_next_line.h
@@ -6,16 +6,8 @@
 /*   By: mamateo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/10 18:47:21 by mamateo           #+#    #+#             */
-/*   Updated: 2018/12/11 22:12:39 by mamateo          ###   ########.fr       */
+/*   Updated: 2018/12/10 18:47:23 by mamateo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-# include <libft.h>
-# include <stdlib.h>
-# include <unistd.h>
 
-# BUF_SIZE 4
-
-#endif

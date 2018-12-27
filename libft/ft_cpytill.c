224e2fe822e3988fdae6b2831354a4f216b407b4
Matthew Mateo <mmatteo36@gmail.com>
2018-12-27 15:52:13 -0800

    Small update

--- libft/ft_cpytill.c
+++ libft/ft_cpytill.c
@@ -1,15 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   ft_cpytill.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mamateo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/10 18:46:41 by mamateo           #+#    #+#             */
-/*   Updated: 2018/12/10 23:24:37 by mamateo          ###   ########.fr       */
+/*   Created: 2018/12/27 15:27:21 by mamateo           #+#    #+#             */
+/*   Updated: 2018/12/27 15:27:55 by mamateo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	get_next_line(const int fd, char **line)
+#include "libft.h"
+
+int			ft_copyuntil(char **dst, char *src, char c)
 {
+	int		i;
+	int		count;
+	int		pos;
+
+	i = -1;
+	count = 0;
+	while (src[++i])
+		if (src[i] == c)
+			break ;
+	pos = i;
+	if (!(*dst = ft_strnew(i)))
+		return (0);
+	while (src[count] && count < i)
+	{
+		if (!(*dst = ft_strjoinch(*dst, src[count])))
+			return (0);
+		count++;
+	}
+	return (pos);
 }

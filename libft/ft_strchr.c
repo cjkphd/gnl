27bc65acfdd4f364fb8383d936f45f73414bc119
Matthew Mateo <mmatteo36@gmail.com>
2018-12-28 16:48:35 -0800

    Modis

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mamateo <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/14 17:30:51 by mamateo           #+#    #+#             */
-/*   Updated: 2018/12/10 11:59:47 by mamateo          ###   ########.fr       */
+/*   Updated: 2018/12/28 12:16:16 by mamateo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,20 @@ char	*ft_strchr(const char *str, int c)
 	int	i;
 
 	i = 0;
-	if (c == '\0')
-	{
-		while (str[i] != '\0')
-			i++;
-		return ((char *)str + i);
-	}
 	while (str[i])
 	{
 		if (str[i] == c)
 			return ((char *)str + i);
 		i++;
 	}
+	if (str[i] == c)
+		return ((char *)str + i);
 	return (NULL);
 }
 
 /*
-** locates a char in a string
-** &str[i] address vs *str[i]
+** if (c == '\0')
+** while (str[i] != '\0')
+** i++;
+** return ((char *)str + i));
 */
